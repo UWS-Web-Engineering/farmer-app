c96ed041ee533174ea5c57654b4e8714d63626c9
Betina Bondoc <betinabondoc@gmail.com>
2021-04-27 14:14:01 +1000

    set up css files, layout blade template, user blade, dashboard blade, and Svelte. Svelte still not working properly.

---
+++ resources/views/user.blade.php
@@ -0,0 +1,8 @@
+@extends ('layout')
+
+@section('content')
+	<div class="container">
+		Welcome to the Farmer App!
+		This is the view for user registration, log in, account settings, and logout.
+	</div>
+@endsection
