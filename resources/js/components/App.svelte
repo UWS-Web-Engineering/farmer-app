246bcdef607892914283ca1e954e05dcb679e9d2
Pia Satuito <pia.satuito@appetiser.com.au>
2021-05-11 21:06:17 +1000

    Added needed js, svelte files, bootstrap

---
+++ resources/js/components/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from "svelte";
+
+	onMount(() => {
+	  console.log("the component has mounted");
+	});
+</script>
+
+<main>
+	<div class="container">
+		<main>
+	<h1>Hello Farmer!</h1>
+	<p>Welcome to your page</p>
+	<p>Here is the list of all crops you are cultivating</p>
+</main>
+	</div>
+</main>
