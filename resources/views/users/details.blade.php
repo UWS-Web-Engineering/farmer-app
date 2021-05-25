6f885e5fef80bde3c91a8ede9a075f100bf644f1
Atoosa <atosa_zian@yahoo.com>
2021-05-25 15:02:51 +1000

    added details functionality

--- resources/views/users/details.blade.php
+++ resources/views/users/details.blade.php
@@ -1,7 +1,7 @@
 @extends ('layout')
 
 @section('content')
-	<div class="a-container a-flex a-flex-column a-justify-content-center">
+	<form name="detailsForm" class="a-container a-flex a-flex-column a-justify-content-center">
 	<svg  class="a-align-self-center" width="114" height="114" viewBox="0 0 114 114" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M14.25 21.375H0C0 48.9176 22.3324
  71.25 49.875 71.25V103.312C49.875 105.272 
@@ -14,22 +14,55 @@
 	/> 
 </svg>
 		<h1 class="a-align-selft-center a-text-center a-text-light a-mb--xl">FARMER APP</h1>
-	
+		<input type="email" name="farmerEmail" required placeholder="Email Address" class="a-text-field a-mb" />
+		<input type="text" name="farmerName" required placeholder="Name" class="a-text-field a-mb" />
+    	<input type="phone" name="farmerPhone" required placeholder="Phone Number" class="a-text-field a-mb" />
+    	<input type="text" name="farmerAddr" required placeholder="Address" class="a-text-field a-mb" />
+		<input type="submit" name="submitBtn" value="SUBMIT" class="a-btn a-ml-auto a-mt a-mb--xl a-align-self-end" />
+	</form>
+	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+	<script>
+	console.log(axios);
+		// Read farmerID from URL
+		const urlParams = new URLSearchParams(window.location.search);
+		const farmerID = urlParams.get('farmerID');
+		// Define form variable
+		let form = document.forms['detailsForm'];
+		// Call submit function on form submission
+		form.addEventListener("submit", submit);
+		async function submit(e) {
+			if (!farmerID) {
+				alert("No farmer defined to the details for!")
+				return void(0);
+			}
+			// Prevent default form submission
+			e.preventDefault();
 
-        <form action="/action_page.php">
-        <input type="text" placeholder="First Name" class="a-text-field a-mb" />
-		<input type="text" placeholder="Last Name" class="a-text-field a-mb" />
-<br>
-  <label class="a-text-light a-text-field a-mb" for="villages">Village:</label>
-  <select name="villages" id="villages">
-    <option value="Sydney">Sydney</option>
-    <option value="Wollongong">Wollongong</option>
-    <option value="c">c</option>
-    <option value="d">d</option>
-  </select>
-  <br><br>
-  <input class="a-btn a-mt a-mb--xl a-align-self-end" type="submit" value="Submit">
-</form>
+			// Check if the form is valid
+			if (form.checkValidity()) {
+				// Disable submit button
+				form.submitBtn.disabled = true;
+				form.submitBtn.value = "Please wait...";
 
-	</div>
+				try {
+					// Create from data from form
+					const farmer = new FormData(form);
+
+					// Send PATCH request to backend
+					const response = await axios.post(
+						`/api/mock/farmers/${farmerID}`,
+						farmer,
+						{ params: { _method: 'PATCH' } });
+					// Redirect to /crops
+					window.location.replace('/crops');
+				} catch(e) {
+					alert(e.message);
+				} finally {
+					// Renable submit button
+					form.submitBtn.value = "SUBMIT"
+					form.submitBtn.disabled = false
+				}
+			}
+		}
+	</script>
 @endsection
